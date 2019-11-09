9de3ba5de22b7afed10480f5d7dd8580b4bb93b8
Sora2455 <boreenpt@gmail.com>
2019-11-10 07:16:52 +1000

    Make web push client static, method async, and split the exception handler based on likely issue

--- SorasNerdDen/Controllers/PushController.cs
+++ SorasNerdDen/Controllers/PushController.cs
@@ -5,11 +5,14 @@
     using SorasNerdDen.Constants;
     using SorasNerdDen.Settings;
     using System;
+    using System.Threading.Tasks;
     using WebPush;
 
     public class PushController : Controller
     {
         private readonly IOptionsSnapshot<VapidSettings> vapidSettings;
+
+        private static readonly WebPushClient webPushClient = new WebPushClient();
 
         public PushController(IOptionsSnapshot<VapidSettings> vapidSettings)
         {
@@ -24,7 +27,14 @@
 
         public class PushSubscriptionKeys
         {
+            /// <summary>
+            /// A secret unique to this push subscription
+            /// </summary>
             public string auth;
+            /// <summary>
+            /// The public key for this push subscription
+            /// (the private key remains on the browser)
+            /// </summary>
             public string p256dh;
         }
 
@@ -32,32 +42,29 @@
         /// The C# model of JSON data posted when push notifications are being subscribed to,
         /// unsubscribed from, or updated
         /// </summary>
-        public class PushSubscriptionChangeModel
+        public class PushSubscriptionModel
         {
             /// <summary>
             /// The URL the push subscription calls to deliver the push message
             /// </summary>
             public string endpoint;
-            public PushSubscriptionKeys keys;
             /// <summary>
-            /// The timestamp the push subscription will expire (if any)
+            /// The encryption keys for this push subscription
             /// </summary>
-            public long? expires;
+            public PushSubscriptionKeys keys;
             public string oldEndpoint;
             public string newEndpoint;
         }
 
         [HttpPost("push/subscribe", Name = PushControllerRoute.Subscribe)]
-        public IActionResult Subscribe([FromBody] PushSubscriptionChangeModel model)
+        public async Task<IActionResult> Subscribe([FromBody] PushSubscriptionModel model)
         {
             var subscription = new PushSubscription(model.endpoint, model.keys?.p256dh, model.keys?.auth);
             VapidDetails vapidDetails = new VapidDetails("mailto:example@example.com",
                 vapidSettings.Value.PublicKey, vapidSettings.Value.PrivateKey);
-            //options["gcmAPIKey"] = @"[your key here]";
-            var webPushClient = new WebPushClient();
             try
             {
-                webPushClient.SendNotification(
+                await webPushClient.SendNotificationAsync(
                     subscription,
                     "{\"title\":\"It works!\",\"message\":\"Thank you for enabling live updates.\"}",
                     vapidDetails
@@ -65,19 +72,39 @@
             }
             catch (WebPushException exception)
             {
-                Console.WriteLine("Http STATUS code" + exception.StatusCode);
+                switch ((int) exception.StatusCode)
+                {
+                    case 400:
+                        Console.WriteLine("Invalid request (usually malformed headers)");
+                        break;
+                    case 404:
+                        Console.WriteLine("Subscription not found (has expired, we should forget about it)");
+                        break;
+                    case 410:
+                        Console.WriteLine("Subscription gone (has expired, we should forget about it)");
+                        break;
+                    case 413:
+                        Console.WriteLine("Payload too large (max payload size is 4kb)");
+                        break;
+                    case 429:
+                        Console.WriteLine("Too many requests");
+                        break;
+                    default:
+                        Console.WriteLine("Http STATUS code" + exception.StatusCode);
+                        break;
+                }
             }
             return new EmptyResult();
         }
 
         [HttpPost("push/unsubscribe", Name = PushControllerRoute.Unsubscribe)]
-        public IActionResult Unubscribe([FromBody] PushSubscriptionChangeModel model)
+        public IActionResult Unubscribe([FromBody] PushSubscriptionModel model)
         {
             return new EmptyResult();
         }
 
         [HttpPost("push/update", Name = PushControllerRoute.Update)]
-        public IActionResult Update([FromBody] PushSubscriptionChangeModel model)
+        public IActionResult Update([FromBody] PushSubscriptionModel model)
         {
             return new EmptyResult();
         }
